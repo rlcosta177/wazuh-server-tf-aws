b5a160e612ac0c7b450f4c28e3971bfb6a157c54
ratsoc camil <rlcosta177@gmail.com>
2024-03-28 17:49:13 -0100

    added the rfc1918 so that the machines can communicate between each other

--- controller/main.tf
+++ controller/main.tf
@@ -149,6 +149,22 @@ resource "aws_security_group" "allow-home-traffic" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+     ingress {
+      cidr_blocks      = [
+        "192.168.0.0/16",
+        "172.16.0.0/12",
+        "10.0.0.0/8",
+      ]
+      description      = "RFC 1918"
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = true
+      to_port          = 0
+    }
+
     egress {
         from_port = 0
         to_port = 0
